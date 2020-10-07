4eb664aa4f8780fd9934f338ff2bee6176a7ac64
David Chipana <chipana_david@hotmail.com>
2020-10-07 20:43:00 -0300

    Iniciando projeto

---
+++ TakeProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TakeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
